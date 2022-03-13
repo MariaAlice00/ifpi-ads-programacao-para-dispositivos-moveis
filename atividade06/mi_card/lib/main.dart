6cbec415e2c7a99f3ed9d90422dacbb2a50983c2
MariaAlice00 <mariaalice021200@gmail.com>
2022-03-13 17:35:56 -0300

    mi_card

--- atividade06/mi_card/lib/main.dart
+++ atividade06/mi_card/lib/main.dart
@@ -1,30 +1,32 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color.fromARGB(255, 207, 162, 188),
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50,
-                backgroundImage: AssetImage('images/ely.jpg'),
+                backgroundImage: AssetImage('images/avatar.jpg'),
               ),
               Text(
-                'Ely Miranda',
+                'Maria Alice',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     color: Colors.white,
                     fontSize: 40,
-                    fontWeight: FontWeight.bold),
+                    fontWeight: FontWeight.w600),
               ),
               Text(
                 'FLUTTER DEVELOPER',
                 style: TextStyle(
-                    color: Colors.teal.shade100,
+                    color: Colors.purple[900],
                     fontSize: 20,
                     letterSpacing: 2.5,
                     fontWeight: FontWeight.bold),
@@ -33,11 +35,11 @@ void main() {
                 height: 20.0,
                 width: 150.0,
                 child: Divider(
-                  color: Colors.teal.shade100,
+                  color: Colors.purple[900],
                 ),
               ),
               buildCard('+55 86 99999 9999', Icons.phone),
-              buildCard('ely.miranda@ifpi.edu.br', Icons.email),
+              buildCard('alice@email.com', Icons.email),
             ],
           ),
         ),
@@ -52,12 +54,12 @@ Card buildCard(String text, IconData icon) {
       child: ListTile(
         leading: Icon(
           icon,
-          color: Colors.teal,
+          color: Colors.purple[900],
         ),
         title: Text(
           text,
           style: TextStyle(
-            color: Colors.teal,
+            color: Colors.purple[900],
             fontSize: 20.0,
           ),
         ),
