f4cc7243cbaaa6f23a713ec887dbc0e529e7a6ec
Maria Alice <61831459+MariaAlice00@users.noreply.github.com>
2022-05-19 10:49:14 -0300

    Update main.dart

--- atividade12/checkbox/lib/main.dart
+++ atividade12/checkbox/lib/main.dart
@@ -1,58 +1,132 @@
 import 'package:flutter/material.dart';
 
-final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+      title: 'Focus',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: MyWidget(),
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
       ),
+      home: const MyHomePage(title: 'Focus'),
     );
   }
 }
 
-class MyWidget extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
   @override
-  _MyWidgetState createState() => _MyWidgetState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
-  final itens = <Map>[
-    {"nome": "Item 1", "check": false},
-    {"nome": "Item 2", "check": false},
-    {"nome": "Item 3", "check": false},
-    {"nome": "Item 4", "check": false},
-  ];
-
+class _MyHomePageState extends State<MyHomePage> {
+  bool isChecked1 = false;
+  bool isChecked2 = false;
+  bool isChecked3 = false;
+  bool isChecked4 = false;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: itens.length,
-      itemBuilder: (context, index) {
-        return CheckboxListTile(
-          title: Text(itens[index]["nome"]),
-          subtitle: Text(
-            "Este item ${itens[index]["check"] ? "" : "não"} está selecionado",
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.red;
+    }
+    
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          leading: Checkbox(
+            checkColor: Colors.white,
+            fillColor: MaterialStateProperty.resolveWith(getColor),
+            value: isChecked1,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked1 = value!;
+                if (isChecked1 && isChecked2 && isChecked3) {
+                  isChecked4 = true;
+                } else {
+                  isChecked4 = false;
+                }
+              });
+            },
           ),
-          value: itens[index]["check"],
-          onChanged: (newValue) {
-            setState(() {
-              itens[index]["check"] = newValue;
-            });
-          },
-        );
-      },
+          title: const Text('Permito utilizar meu número de celular'),
+        ),
+        ListTile(
+          leading: Checkbox(
+            checkColor: Colors.white,
+            fillColor: MaterialStateProperty.resolveWith(getColor),
+            value: isChecked2,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked2 = value!;
+                if (isChecked1 && isChecked2 && isChecked3) {
+                  isChecked4 = true;
+                } else {
+                  isChecked4 = false;
+                }
+              });
+            },
+          ),
+          title: const Text('Permito utilizar o aúdio do sistema'),
+        ),
+        ListTile(
+          leading: Checkbox(
+            checkColor: Colors.white,
+            fillColor: MaterialStateProperty.resolveWith(getColor),
+            value: isChecked3,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked3 = value!;
+                if (isChecked1 && isChecked2 && isChecked3) {
+                  isChecked4 = true;
+                } else {
+                  isChecked4 = false;
+                }
+              });
+            },
+          ),
+          title: const Text('Aceito compartilhar dados para melhoria do aplicativo'),
+        ),
+        ListTile(
+          leading: Checkbox(
+            checkColor: Colors.white,
+            tristate: false,
+            fillColor: MaterialStateProperty.resolveWith(getColor),
+            value: isChecked4,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked4 = value!;
+                if (isChecked4) {
+                  isChecked1 = true;
+                  isChecked2 = true;
+                  isChecked3 = true;
+                } else {
+                  isChecked1 = false;
+                  isChecked2 = false;
+                  isChecked3 = false;
+                }
+              });
+            },
+          ),
+          title: const Text('Aceitar todos os itens acima'),
+        ),
+      ],
     );
   }
 }
